7a481c49f6edd5ae5e07e29431c428c8b36c5b16
João Ferreira <100808096+JoaoFerreira123@users.noreply.github.com>
2024-08-24 19:45:48 -0300

    pages created and linked

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views #o ponto é pra importar dessa pasta
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    path('profissional/', views.profissional, name='profissional'),
+    path('pessoal/', views.pessoal, name='pessoal'),
 ]
--- base/views.py
+++ base/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'base/home.html' )
+
+def profissional(request):
+    return render(request, 'base/profissional.html')
+
+def pessoal(request):
+    return render(request, 'base/pessoal.html')
